758e3e57e46609f71f755a073e9cbc46df084cfd
ravikrishnudu <ravikrishnuduvenna@gmail.com>
2021-06-03 12:21:56 +0530

    require is added to input

--- src/components/AddStudent.jsx
+++ src/components/AddStudent.jsx
@@ -6,6 +6,11 @@ async function getGrades() {
   return res.data;
 }
 
+async function getStudents() {
+  const response = await axios.get("/students");
+  console.log(response);
+  return response.data;
+}
 function AddStudent() {
   const [name, setName] = useState("");
   const [gender, setGender] = useState("");
@@ -13,15 +18,24 @@ function AddStudent() {
   const [email, setEmail] = useState("");
   const [grade, setGrade] = useState("Grade");
   const [grades, setGrades] = useState("");
+  const [students, setStudents] = useState("");
 
   useEffect(() => {
+    getStudents().then((res) => {
+      setStudents(res);
+    });
     getGrades().then((res) => {
       setGrades(res);
     });
   }, []);
 
+  const addStudents = (student) => {
+    setStudents(...students, student);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     try {
       const response = await axios.post("/student", {
         name,
@@ -30,13 +44,15 @@ function AddStudent() {
         email,
         gradeId: grade,
       });
+      setStudents(...students);
+      addStudents(response.data);
       console.log(response);
     } catch (error) {
       console.log(error);
     }
   };
 
-  // console.log(grade.id);
+  // console.log(students);
 
   return (
     <div>
@@ -56,6 +72,7 @@ function AddStudent() {
                 type="text"
                 className="form-control"
                 placeholder="Name"
+                required
               />
             </div>
             <div className="col">
@@ -65,6 +82,7 @@ function AddStudent() {
                 type="text"
                 className="form-control"
                 placeholder="Gender"
+                required
               />
             </div>
             <div className="col">
@@ -74,6 +92,7 @@ function AddStudent() {
                 type="text"
                 className="form-control"
                 placeholder="Date of Birth"
+                required
               />
             </div>
             <div className="col">
@@ -83,6 +102,7 @@ function AddStudent() {
                 type="text"
                 className="form-control"
                 placeholder="Email"
+                required
               />
             </div>
             <div className="col">
@@ -95,6 +115,7 @@ function AddStudent() {
                 {grades &&
                   grades.map((grade) => (
                     <>
+                      key={grade.id}
                       <option value={grade.id}>{grade.grade}</option>
                     </>
                   ))}
@@ -105,13 +126,6 @@ function AddStudent() {
           </div>
         </form>
       </div>
-      {/* <div className="form-row">
-          <div className="col">
-            <Link to="/previewattendance" className="btn btn-warning">
-              Preview Attendence
-            </Link>
-          </div>
-        </div> */}
     </div>
   );
 }
